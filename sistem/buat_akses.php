7bc7e56ef3770478cbd4eebdb277e30f4a6ccdad
JackWhiters <73758793+JackWhiters@users.noreply.github.com>
2022-05-26 01:48:02 +0700

    fix bugs 2

--- sistem/buat_akses.php
+++ sistem/buat_akses.php
@@ -157,7 +157,7 @@ $query2 = mysqli_query($koneksi, "SELECT kode_akses FROM tbl_akses WHERE nim='$n
         <!-- Sidebar Start -->
         <div class="sidebar pe-4 pb-3">
             <nav class="navbar bg-light navbar-light">
-                <a href="index.html" class="navbar-brand mx-4 mb-3">
+                <a href="index.php" class="navbar-brand mx-4 mb-3">
                     <h3 class="text-primary"><img class="rounded-circle" src="logo.png" alt="" style="width: 40px; height: 40px;"></i> E-PEMIRA</h3>
                 </a>
 
