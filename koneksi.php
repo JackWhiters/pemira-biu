c0bee93af3ab706b569dc48f4346f1d4b22e7612
aldisyah02 <aldisyahrahman@gmail.com>
2022-05-26 08:44:23 +0700

    Update Database

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php 
 
-$koneksi = mysqli_connect("localhost","root","","disstore_pemira");
+$koneksi = mysqli_connect("localhost","disstore_pemira","jambubiji123","disstore_pemira");
 
 if (mysqli_connect_error()){
 	echo "koneksi database gagal" .mysqli_connect_error();
