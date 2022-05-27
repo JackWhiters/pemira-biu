ecfa4d2f9db9fa7440c26475772a939b07e05da4
aldisyah02 <aldisyahrahman@gmail.com>
2022-05-27 16:56:04 +0700

    edit file koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php 
 
-$koneksi = mysqli_connect("localhost","disstore_pemira","jambubiji123","disstore_pemira");
+$koneksi = mysqli_connect("localhost","bemf1323_pemira","jambubiji123","bemf1323_pemira");
 
 if (mysqli_connect_error()){
 	echo "koneksi database gagal" .mysqli_connect_error();
